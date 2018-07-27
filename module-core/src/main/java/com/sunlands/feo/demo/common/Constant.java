fdab16904c1c8f58708e35803252bd06f994443e
chenlei <chenlei09@sunlands.com>
2018-07-27 10:43:09 +0800

    修改目录

---
+++ module-core/src/main/java/com/sunlands/feo/demo/common/Constant.java
@@ -0,0 +1,12 @@
+package com.sunlands.feo.demo.common;
+
+/**
+ * @Project : websocket-feo
+ * @Package Name : com.sunlands.feo.demo.common
+ * @Description : 常量
+ * @Author : chenlei
+ * @Create Date : 2018年07月27日 10:34
+ * ------------    --------------    ---------------------------------
+ */
+public class Constant {
+}
